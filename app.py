b296804052be3a009e0065c0dfc90d603d99f770
Chris Nunez <84637156+santonode@users.noreply.github.com>
2025-10-08 12:05:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from flask import Flask
+from wurdle import wurdle_bp
+from memes import memes_bp
 
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+app.register_blueprint(wurdle_bp)
+app.register_blueprint(memes_bp, url_prefix='/memes')
+
+if __name__ == '__main__':
+    app.run(debug=True)
