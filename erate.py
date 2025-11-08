d70356d5078523d6a0b754df7abaa5473f6e1b01
Chris Nunez <84637156+santonode@users.noreply.github.com>
2025-11-08 18:25:49 -0500

    Update erate.py

--- erate.py
+++ erate.py
@@ -215,7 +215,7 @@ def _import_one_record():
                     row.get('Billed Entity Phone Ext',''),
                     int(row.get('Number of Eligible Entities') or 0),
                     row.get('Contact Name',''),
-                    row.get('Contact Address 11',''),
+                    row.get('Contact Address 1',''),
                     row.get('Contact Address 2',''),
                     row.get('Contact City',''),
                     row.get('Contact State',''),
