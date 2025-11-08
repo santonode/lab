04752c58afec1efa2460f01b6b823dd659730ef7
Chris Nunez <84637156+santonode@users.noreply.github.com>
2025-11-08 15:32:50 -0500

    Update patch_psycopg2.py

--- patch_psycopg2.py
+++ patch_psycopg2.py
@@ -1,24 +1,19 @@
 # patch_psycopg2.py
-import importlib
 import sys
 
-# === PATCH BEFORE ANYTHING ELSE ===
-def patch_psycopg2():
-    from psycopg import Connection
+# Fake psycopg2 that uses psycopg v3
+class FakePsycopg2:
+    @staticmethod
+    def connect(*args, **kwargs):
+        from psycopg import connect
+        return connect(*args, **kwargs)
 
-    # Fake psycopg2 module
-    class FakePsycopg2:
-        @staticmethod
-        def connect(*args, **kwargs):
-            from psycopg import connect
-            return connect(*args, **kwargs)
+    # Fake exceptions
+    class OperationalError(Exception): pass
+    class IntegrityError(Exception): pass
+    class InterfaceError(Exception): pass
+    class DatabaseError(Exception): pass
+    class ProgrammingError(Exception): pass
 
-        # Add any other needed attributes
-        OperationalError = type('OperationalError', (Exception,), {})
-        IntegrityError = type('IntegrityError', (Exception,), {})
-
-    # Inject fake psycopg2 into sys.modules
-    sys.modules['psycopg2'] = FakePsycopg2
-
-# Run immediately
-patch_psycopg2()
+# INJECT IMMEDIATELY
+sys.modules['psycopg2'] = FakePsycopg2
