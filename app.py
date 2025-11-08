567917d567a828a770c12ae3b0434ee4bfe88c1d
Chris Nunez <84637156+santonode@users.noreply.github.com>
2025-11-08 16:03:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # app.py
 from flask import Flask
-import os
 from models import Base
 from extensions import engine
 from erate import erate_bp
@@ -12,10 +11,9 @@ app.secret_key = os.urandom(24)
 app.register_blueprint(erate_bp)
 app.register_blueprint(memes_bp)
 
-# Create tables
 with app.app_context():
     Base.metadata.create_all(bind=engine)
     init_db()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
